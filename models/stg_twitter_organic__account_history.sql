99b6fc2d9a1427ab6666f99b811cf688c56a83c3
Dylan Baker <dylan@dbanalytics.co>
2021-11-02 19:25:53 +0000

    fix partition

--- models/stg_twitter_organic__account_history.sql
+++ models/stg_twitter_organic__account_history.sql
@@ -45,7 +45,7 @@ is_most_recent as (
 
     select
         *,
-        row_number() over (partition by account_id order by _fivetran_synced desc) = 1 as is_most_recent_record
+        row_number() over (partition by account_id, source_relation order by _fivetran_synced desc) = 1 as is_most_recent_record
     from final
 
 )
